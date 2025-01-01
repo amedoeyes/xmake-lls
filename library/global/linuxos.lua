ecdf9166dc8d0d7d3583913b186e7a0358f49f59
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-01-02 00:58:42 +0200

    feat: add global linuxos

---
+++ library/global/linuxos.lua
@@ -0,0 +1,15 @@
+---@meta
+
+---@class linuxos
+linuxos = {}
+
+---@return string
+function linuxos.name() end
+
+---@return string
+function linuxos.version() end
+
+---@return string
+function linuxos.kernelver() end
+
+return linuxos
