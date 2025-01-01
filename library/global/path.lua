c30330dca48d915c0b24d875d1177885b4c11d07
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-01-01 19:46:25 +0200

    feat: improve global path

--- library/global/path.lua
+++ library/global/path.lua
@@ -2,85 +2,85 @@
 
 path = {}
 
----@param p Path|string
----@param rootdir Path|string|nil
----@return Path
+---@param p string
+---@param rootdir string?
+---@return string
 function path.absolute(p, rootdir) end
 
----@param p Path|string
+---@param p string
 ---@return string
 function path.basename(p) end
 
----@param p Path|string
+---@param p string
 ---@return string
 function path.cygwin(p) end
 
----@param p Path|string
+---@param p string
 ---@return string
 function path.cygwin_path(p) end
 
----@param p Path|string
----@param sep string|nil
+---@param p string
+---@param sep string?
 ---@return string
 function path.directory(p, sep) end
 
 ---@return string
 function path.envsep() end
 
----@param p Path|string
----@param level integer
+---@param p string
+---@param level integer?
 ---@return string
 function path.extension(p, level) end
 
----@param p Path|string
----@param sep string|nil
+---@param p string
+---@param sep string?
 ---@return string
 function path.filename(p, sep) end
 
----@param p Path|string
+---@param p string
 ---@return boolean
 function path.instance_of(p) end
 
----@param p Path|string
+---@param p string
 ---@return boolean
 function path.is_absolute(p) end
 
----@param p Path|string
+---@param p string
 ---@return boolean
 function path.islastsep(p) end
 
----@param p Path|string
----@param ... Path|string
----@return Path
+---@param p string
+---@param ... string
+---@return string
 function path.join(p, ...) end
 
----@param paths Path[]|string[]
----@param envsep string|nil
+---@param paths string[]
+---@param envsep string?
 ---@return string
 function path.joinenv(paths, envsep) end
 
----@param p Path|string
+---@param p string
 ---@param transform any
 ---@return Path
 function path.new(p, transform) end
 
----@param p Path|string
----@return Path
+---@param p string
+---@return string
 function path.normalize(p) end
 
 ---@param pattern string
 ---@return string
 function path.pattern(pattern) end
 
----@param p Path|string
----@param rootdir Path|string
----@return Path
+---@param p string
+---@param rootdir string?
+---@return string
 function path.relative(p, rootdir) end
 
 ---@return string
 function path.sep() end
 
----@param p Path|string
+---@param p string
 ---@return string[]
 function path.split(p) end
 
@@ -88,11 +88,11 @@ function path.split(p) end
 ---@return string[]
 function path.splitenv(env_path) end
 
----@param p Path|string
----@param opt table|nil
----@return Path
+---@param p string
+---@param opt table?
+---@return string
 function path.translate(p, opt) end
 
----@param p Path|string
+---@param p string
 ---@return string
 function path.unix(p) end
