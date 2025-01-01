32741e308e4978af290feed4d1fe96ffb855e6b0
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-01-02 00:54:41 +0200

    feat: add global debug

---
+++ library/global/debug.lua
@@ -0,0 +1,11 @@
+---@meta
+
+---@class debug
+debug = {}
+
+debug.collectgarbage = collectgarbage
+debug.rawequal = rawequal
+debug.rawget = rawget
+debug.rawlen = rawlen
+debug.rawset = rawset
+debug.require = require
