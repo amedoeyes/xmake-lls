9ba041cdfba804ecc79d143bdd59deda3c91e2ad
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-01-02 01:20:49 +0200

    feat: add global xmake

---
+++ library/global/xmake.lua
@@ -0,0 +1,25 @@
+---@meta
+
+---@class xmake
+xmake = {}
+
+---@return string
+function xmake.arch() end
+
+---@return string[]
+function xmake.argv() end
+
+---@return string
+function xmake.branch() end
+
+---@return boolean
+function xmake.luajit() end
+
+---@return string
+function xmake.programdir() end
+
+---@return string
+function xmake.programfile() end
+
+---@return string
+function xmake.version() end
