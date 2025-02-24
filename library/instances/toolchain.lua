0bc968c8af9d32dd24d19c405f235cf3d48b71de
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-24 20:12:19 +0200

    feat: update toolchain instance

--- library/instances/toolchain.lua
+++ library/instances/toolchain.lua
@@ -6,207 +6,114 @@
 ---@class Toolchain
 local Toolchain = {}
 
----
----Get name
----
 ---@return ToolchainName
 function Toolchain:name() end
 
----
----Get platform name
----
+---@return string
+function Toolchain:namespace() end
+
+---@return string
+function Toolchain:fullname() end
+
 ---@return PlatformName
 function Toolchain:plat() end
 
----
----Set platform
----
----@param plat Platform
+---@param plat PlatformName
 function Toolchain:plat_set(plat) end
 
----
----Get architecture
----
 ---@return Architecture
 function Toolchain:arch() end
 
----
----Set architecture
----
 ---@param arch Architecture
 function Toolchain:arch_set(arch) end
 
----
----Check platforms
----
 ---@param ... PlatformName
 ---@return boolean
 function Toolchain:is_plat(...) end
 
----
----Check architectures
----
 ---@param ... Architecture
 ---@return boolean
 function Toolchain:is_arch(...) end
 
----
----Get info
----
 ---@return table
 function Toolchain:info() end
 
----
----Set value
----
 ---@param name string
 ---@param ... any
 function Toolchain:set(name, ...) end
 
----
----Add value
----
 ---@param name string
 ---@param ... any
 function Toolchain:add(name, ...) end
 
----
----Get value
----
 ---@param name string
+---@param opt table?
 ---@return any
 function Toolchain:get(name, opt) end
 
----
----Get kind
----
 ---@return string
 function Toolchain:kind() end
 
----
----Get formats
----
----@return table
+---@return any
 function Toolchain:formats() end
 
----
----Check if cross
----
 ---@return boolean
 function Toolchain:is_cross() end
 
----
----Check if standalone
----
 ---@return boolean
 function Toolchain:is_standalone() end
 
----
----Check if global
----
 ---@return boolean
 function Toolchain:is_global() end
 
----
----Check if builtin
----
 ---@return boolean
 function Toolchain:is_builtin() end
 
----
----Get runtime environment
----
----@return table|nil
+---@return table?
 function Toolchain:runenvs() end
 
----
----Get program path and name of tool
----
 ---@param toolkind ToolKind
 ---@return string, string
 function Toolchain:tool(toolkind) end
 
----
----Get script
----
 ---@param name string
 ---@return any
 function Toolchain:script(name) end
 
----
----Get cross
----
 ---@return any
 function Toolchain:cross() end
 
----
----Get bin directory
----
 ---@return string
 function Toolchain:bindir() end
 
----
----Get sdk directory
----
 ---@return string
 function Toolchain:sdkdir() end
 
----
----Get cache key
----
 ---@return string
 function Toolchain:cachekey() end
 
----
----Get config
----
+---@param name string
 ---@return any
 function Toolchain:config(name) end
 
----
----Set config
----
 ---@param name string
 ---@param data any
 function Toolchain:config_set(name, data) end
 
----
----Save config
----
 function Toolchain:configs_save() end
 
----
----Check
----
 ---@return table
 function Toolchain:check() end
 
----
----Load
----
 function Toolchain:load() end
 
----
----Check cross
----
----@return any
+---@return table
 function Toolchain:check_cross_toolchain() end
 
----
----Load cross
----
----@return any
 function Toolchain:load_cross_toolchain() end
 
----
----Get packages
----
----@return table|nil
+---@return table?
 function Toolchain:packages() end
 
----
----Save to file
----
 ---@param filepath string
----@return any
 function Toolchain:savefile(filepath) end
